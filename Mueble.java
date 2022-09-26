e484b24a78cfe889075b9b3c20c73ee6cf26737f
JanMarco <marcovarela1290@gmail.com>
2022-09-26 08:07:56 -0500

    Actividades 26 de Septiembre
    8:07am

---
+++ Mueble.java
@@ -0,0 +1,10 @@
+public class Mueble{
+    private int patas;
+    private float costo;
+    private String descripcion;
+    public Mueble(int patas, float costo, String descripcion){
+        this.patas = patas;
+        this.costo = costo;
+        this.descripcion = descripcion;
+    }
+}
